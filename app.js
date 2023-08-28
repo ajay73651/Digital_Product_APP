c5c8c4f5cc5a8005e1cd481408ea645e3f4860e9
Obaid Aslam <ajay73651@gmail.com>
2023-08-29 00:38:48 +0530

    Dynamic Routes Part 3 - delete prodcut implementation

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorController = require('./controllers/error')
+
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
--- routes/admin.js
+++ routes/admin.js
@@ -15,4 +15,13 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+//route for edit product
+router.post('/edit-product', adminController.postEditProduct);
+
+
+// route for delete product
+router.post('/delete-product', adminController.postDeleteProduct);
+
 module.exports = router;
