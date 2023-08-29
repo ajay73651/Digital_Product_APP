9f383de47d861396161b753e8d859a21367286e4
Obaid Aslam <ajay73651@gmail.com>
2023-08-29 23:46:48 +0530

    Starting with MYSQL

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const errorController = require('./controllers/error');
-
+const db = require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,6 +11,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+db.execute('select * from products');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
